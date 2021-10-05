ecd67864c76166aa7d86a860af63a1473abcd081
Julius Kiano <jgkiano@gmail.com>
2021-10-05 18:36:18 +0300

    exported init script

--- src/OkCore/index.ts
+++ src/OkCore/index.ts
@@ -1,5 +1,36 @@
+import { OkHiNativeModule } from '../OkHiNativeModule';
+import type { OkHiApplicationConfiguration } from './types';
+import type { AuthApplicationConfig } from './_types';
+
 export * from './types';
 export * from './OkHiException';
 export * from './OkHiMode';
 export * from './Helpers';
-export { configureApplication } from './OkHiAuth';
+
+let okhiApplicationConfiguration: OkHiApplicationConfiguration | undefined;
+
+export function initialize(configuration: OkHiApplicationConfiguration) {
+  return new Promise<void>((resolve, _reject) => {
+    okhiApplicationConfiguration = configuration;
+    resolve();
+  });
+}
+
+export async function getApplicationConfiguration(): Promise<AuthApplicationConfig | null> {
+  try {
+    if (typeof okhiApplicationConfiguration === 'object') {
+      return {
+        ...okhiApplicationConfiguration,
+        auth: {
+          token: await OkHiNativeModule.getAuthToken(
+            okhiApplicationConfiguration?.credentials.branchId || '',
+            okhiApplicationConfiguration?.credentials.clientKey || ''
+          ),
+        },
+      };
+    }
+    return null;
+  } catch (error) {
+    throw error;
+  }
+}
