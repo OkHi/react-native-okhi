7f6f6ee5e31fd5de9008d322648b5d12c92e6cd2
Julius Kiano <jgkiano@gmail.com>
2021-10-05 18:34:33 +0300

    added initialize to OkHiNativeModuleType

--- src/OkHiNativeModule/index.ts
+++ src/OkHiNativeModule/index.ts
@@ -1,4 +1,5 @@
 import { NativeModules } from 'react-native';
+import type { OkHiInitializationConfiguration } from '../OkCore/_types';
 
 type OkHiNativeModuleType = {
   multiply(a: number, b: number): Promise<number>;
@@ -12,6 +13,8 @@ type OkHiNativeModuleType = {
   requestEnableGooglePlayServices(): Promise<boolean>;
   getSystemVersion(): Promise<number | string>;
   getApplicationConfiguration(): Promise<string>;
+  getAuthToken(branchId: string, clientKey: string): Promise<string>;
+  initialize(config: OkHiInitializationConfiguration): Promise<void>;
 };
 
 export const OkHiNativeModule: OkHiNativeModuleType = NativeModules.Okhi;
