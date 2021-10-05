a1416486af8278fed41dfb93b2176f3cfe9f2fe8
Julius Kiano <jgkiano@gmail.com>
2021-10-05 18:38:47 +0300

    added init script

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,16 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import * as OkHi from 'react-native-okhi';
+
+OkHi.initialize({
+  credentials: {
+    branchId: '',
+    clientKey: '',
+  },
+  context: {
+    mode: 'dev' as any,
+  },
+}).catch(console.log);
 
 AppRegistry.registerComponent(appName, () => App);
