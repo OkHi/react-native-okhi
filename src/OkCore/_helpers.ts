1e7fd6c8b1b512183c2c03e19b4667e5c5fcb6a7
Julius Kiano <jgkiano@gmail.com>
2021-10-15 09:50:15 +0300

    isValidPlatform to handle errors

--- src/OkCore/_helpers.ts
+++ src/OkCore/_helpers.ts
@@ -3,14 +3,32 @@ import { OkHiException } from './';
 
 const SUPPORTED_PLATFORMS = ['ios', 'android'];
 
-export function isValidPlatform<T>(fn: () => T, platform?: PlatformOSType) {
-  const error = new OkHiException({
-    code: OkHiException.UNSUPPORTED_PLATFORM_CODE,
-    message: OkHiException.UNSUPPORTED_PLATFORM_MESSAGE,
+export function isValidPlatform<T>(
+  fn: () => Promise<T>,
+  platform?: PlatformOSType
+) {
+  return new Promise<T>((resolve, reject) => {
+    const error = new OkHiException({
+      code: OkHiException.UNSUPPORTED_PLATFORM_CODE,
+      message: OkHiException.UNSUPPORTED_PLATFORM_MESSAGE,
+    });
+    if (platform && Platform.OS !== platform) {
+      reject(error);
+    } else if (!SUPPORTED_PLATFORMS.includes(Platform.OS)) {
+      reject(error);
+    } else {
+      fn()
+        .then(resolve)
+        .catch((error) => {
+          reject(
+            new OkHiException({
+              code: error.code || OkHiException.UNKNOWN_ERROR_CODE,
+              message: error.message || OkHiException.UNKNOWN_ERROR_MESSAGE,
+            })
+          );
+        });
+    }
   });
-  if (platform && Platform.OS !== platform) throw error;
-  if (!SUPPORTED_PLATFORMS.includes(Platform.OS)) throw error;
-  return fn();
 }
 
 export function errorHandler<T>(fn: () => Promise<T>) {
