dcf360de9447a87634188f8bce0b94ce40e36cdf
Julius Kiano <jgkiano@gmail.com>
2021-10-16 17:36:08 +0300

    created canStartAddressCreation

--- src/OkCollect/Helpers.ts
+++ src/OkCollect/Helpers.ts
@@ -1,9 +1,13 @@
+import { Platform } from 'react-native';
 import {
   isLocationServicesEnabled,
   isLocationPermissionGranted,
+  isGooglePlayServicesAvailable,
+  OkHiException,
   requestEnableLocationServices,
-  requestLocationPermission,
-} from '../';
+  requestEnableGooglePlayServices,
+} from '../OkCore';
+import { requestLocationPermission } from '../OkCore';
 
 /**
  * Checks whether all necessary permissions and services are available in order to start the address creation process.
@@ -11,31 +15,38 @@ import {
  * @param {boolean} configuration.requestServices Flag that determines whether to request the services from the user.
  * @returns {Promise<boolean>} A promise that resolves to a boolean value indicating whether or not all conditions are met to start the address creation process.
  */
-export const canStartAddressCreation = async (configuration?: {
+export const canStartAddressCreation = (configuration?: {
   requestServices?: boolean;
 }): Promise<boolean> => {
   return new Promise(async (resolve, reject) => {
-    try {
-      let hasLocationServices = await isLocationServicesEnabled();
-      let hasLocationPermission = await isLocationPermissionGranted();
-      if (!configuration?.requestServices) {
-        return resolve(hasLocationPermission && hasLocationServices);
-      }
-      if (!hasLocationServices) {
-        hasLocationServices = await requestEnableLocationServices();
-        if (!hasLocationServices) {
-          return resolve(hasLocationServices);
-        }
-      }
-      if (!hasLocationPermission) {
-        hasLocationPermission = await requestLocationPermission();
-        if (!hasLocationPermission) {
-          return resolve(hasLocationPermission);
-        }
-      }
-      return resolve(hasLocationPermission && hasLocationServices);
-    } catch (error) {
-      reject(error);
+    const requestServices = configuration && configuration.requestServices;
+    const locationServicesStatus = await isLocationServicesEnabled();
+    const googlePlayServices =
+      Platform.OS === 'android' ? await isGooglePlayServicesAvailable() : true;
+    const locationPerm = await isLocationPermissionGranted();
+    if (!requestServices) {
+      resolve(locationServicesStatus && googlePlayServices && locationPerm);
+    }
+    if (!locationServicesStatus && Platform.OS === 'ios') {
+      reject(
+        new OkHiException({
+          code: OkHiException.SERVICE_UNAVAILABLE_CODE,
+          message: 'Location services is unavailable',
+        })
+      );
+    } else {
+      const locationServicesRequestStatus =
+        (await requestEnableLocationServices()) as boolean;
+      const googlePlayServicesRequestStatus =
+        Platform.OS === 'android'
+          ? await requestEnableGooglePlayServices()
+          : true;
+      const locationPermStatus = await requestLocationPermission();
+      resolve(
+        locationServicesRequestStatus &&
+          googlePlayServicesRequestStatus &&
+          locationPermStatus
+      );
     }
   });
 };
