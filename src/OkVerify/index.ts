bea2a4f6c8efaa08f5de4fb8b6e7d1ea4ec68660
Julius Kiano <jgkiano@gmail.com>
2021-10-20 16:19:07 +0300

    added comments, request whenInUse location permission

--- src/OkVerify/index.ts
+++ src/OkVerify/index.ts
@@ -9,10 +9,22 @@ import {
   requestEnableGooglePlayServices,
   requestEnableLocationServices,
 } from '../';
+import { requestLocationPermission } from '../../lib/typescript';
 import { errorHandler, isValidPlatform } from '../OkCore/_helpers';
 import { OkHiNativeModule } from '../OkHiNativeModule';
 import type { OkVerifyStartConfiguration } from './types';
 
+/**
+ * Starts verification for a particular address
+ * @param {string} phoneNumber A users phone number
+ * @param {string} locationId An OkHi location identifier obtained after successfull creation of addresses.
+ * @param {number} lat The latitude of the created address
+ * @param {number} lon The longitude of the created address
+ * @param {Object} configuration Configures how verification will start on different platforms
+ * @param {Object} configuration.android Specifices how verification will start on Android platforms
+ * @param {boolean} configuration.android.withForeground Specifices if the foreground service will be turned on to speed up rate of verification
+ * @returns {Promise<string>} A promise that resolves to a string value of the location identifier
+ */
 export const start = (
   phoneNumber: string,
   locationId: string,
@@ -40,6 +52,14 @@ export const start = (
   });
 };
 
+/**
+ * Starts verification for a particular address using the response object returned by OkCollect
+ * @param {Object} response Response returned by OkCollect
+ * @param {Object} configuration Configures how verification will start on different platforms
+ * @param {Object} configuration.android Specifices how verification will start on Android platforms
+ * @param {boolean} configuration.android.withForeground Specifices if the foreground service will be turned on to speed up rate of verification
+ * @returns {Promise<string>} A promise that resolves to a string value of the location identifier
+ */
 export const startVerification = async (
   response: OkCollectSuccessResponse,
   configuration?: OkVerifyStartConfiguration
@@ -76,12 +96,22 @@ export const startVerification = async (
   });
 };
 
+/**
+ * Stops verification for a particular address using a user's phonenumber and OkHi location identifier
+ * @param {string} phoneNumber The user's phone number
+ * @param {string} locationId An OkHi location identifier obtained after successfull creation of addresses.
+ * @returns {Promise<string>} A promise that resolves to a string value of the location identifier
+ */
 export const stopVerification = (phoneNumber: string, locationId: string) => {
   return isValidPlatform(() =>
     OkHiNativeModule.stopAddressVerification(phoneNumber, locationId)
   );
 };
 
+/**
+ * Android Only - Starts a foreground service that speeds up rate of verification
+ * @returns {Promise<boolean>} A promise that resolves to a boolean value indicating whether the service has started successfully
+ */
 export const startForegroundService = () => {
   return isValidPlatform(
     () => errorHandler(OkHiNativeModule.startForegroundService),
@@ -89,10 +119,18 @@ export const startForegroundService = () => {
   );
 };
 
+/**
+ * Android Only - Stops previously started foreground services
+ * @returns {Promise<boolean>} A promise that resolves to a boolean value indicating whether the service has stopped successfully
+ */
 export const stopForegroundService = () => {
   return isValidPlatform(OkHiNativeModule.stopForegroundService, 'android');
 };
 
+/**
+ * Android Only - Checks if the foreground service is running
+ * @returns {Promise<boolean>} A promise that resolves to a boolean value indicating whether the service is running
+ */
 export const isForegroundServiceRunning = () => {
   return isValidPlatform(
     OkHiNativeModule.isForegroundServiceRunning,
@@ -100,6 +138,12 @@ export const isForegroundServiceRunning = () => {
   );
 };
 
+/**
+ * Checks whether all necessary permissions and services are available in order to start the address verification process
+ * @param {Object} configuration Object that determines whether or not to request these permissions and services from the user
+ * @param {boolean} configuration.requestServices Flag that determines whether to request the services from the user
+ * @returns {Promise<boolean>} A promise that resolves to a boolean value indicating whether or not all conditions are met to start the address verification process
+ */
 export const canStartVerification = (configuration?: {
   requestServices?: boolean;
 }): Promise<boolean> => {
@@ -131,11 +175,13 @@ export const canStartVerification = (configuration?: {
         Platform.OS === 'android'
           ? await requestEnableGooglePlayServices()
           : true;
+      const whenInUseLocationRequestStatus = await requestLocationPermission();
       const backgroundLocationRequestStatus =
         await requestBackgroundLocationPermission();
       resolve(
         locationServicesRequestStatus &&
           googlePlayServicesRequestStatus &&
+          whenInUseLocationRequestStatus &&
           backgroundLocationRequestStatus
       );
     }
