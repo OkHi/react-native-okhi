f72a5ec5138924a5f8b742d07874781b11719636
jgkiano <jgkiano@gmail.com>
2026-01-13 17:26:21 +0300

    refactor file

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,11 @@
-import { NitroModules } from 'react-native-nitro-modules'
-import type { OkhiNitro as OkhiNitroSpec } from './specs/okhi-nitro.nitro'
-import type {
-  NitroOkCollect,
-  NitroOkHiSuccessResponse,
-  OkCollect,
-  OkHiLogin,
-  OkHiVerificationType,
-} from './types'
-
-const OkhiNitro = NitroModules.createHybridObject<OkhiNitroSpec>('OkhiNitro')
+import { OkHiNitro, startGenericAddressVerification } from './functions'
+import type { OkCollect, OkHiLogin } from './types'
 
 export async function login(
   credentials: OkHiLogin
 ): Promise<string[] | undefined> {
   return new Promise((resolve) => {
-    OkhiNitro.login(credentials, resolve)
-  })
-}
-async function startGenericAddressVerification(
-  type: OkHiVerificationType,
-  params?: {
-    okcollect?: OkCollect
-  }
-): Promise<NitroOkHiSuccessResponse> {
-  return new Promise((resolve, reject) => {
-    const okcollect: NitroOkCollect = {
-      configuration: {
-        streetView: params?.okcollect?.configuration?.streetView || true,
-        withAppBar: params?.okcollect?.configuration?.withAppBar || true,
-        withHomeAddressType:
-          params?.okcollect?.configuration?.withHomeAddressType || true,
-        withWorkAddressType:
-          params?.okcollect?.configuration?.withWorkAddressType || false,
-      },
-      style: {
-        color: params?.okcollect?.style?.color || '#005D67',
-        logo: params?.okcollect?.style?.logo || 'https://cdn.okhi.co/icon.png',
-        name: params?.okcollect?.style?.name || 'OkHi Customer',
-      },
-      locationId:
-        typeof params?.okcollect?.locationId === 'string'
-          ? params.okcollect.locationId
-          : undefined,
-    }
-    OkhiNitro.startAddressVerification(type, okcollect, (response, error) => {
-      if (response) {
-        resolve(response)
-      }
-      if (error) {
-        reject(error)
-      }
-    })
+    OkHiNitro.login(credentials, resolve)
   })
 }
 
