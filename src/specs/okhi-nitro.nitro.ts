844b6a18b04067ee3855457776e5a13a9bb12d48
jgkiano <jgkiano@gmail.com>
2026-01-13 06:27:25 +0300

    added onStart call

--- src/specs/okhi-nitro.nitro.ts
+++ src/specs/okhi-nitro.nitro.ts
@@ -23,4 +23,6 @@ export interface OkhiNitro extends HybridObject<{
     okcollect: NitroOkCollect,
     callback: (response?: OkHiSuccessResponse, error?: OkHiException) => void
   ): void
+
+  onStart(): void
 }
