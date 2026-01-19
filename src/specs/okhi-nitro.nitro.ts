fb9e14a68bda36a3f27fa7fea0aed6effda00093
jgkiano <jgkiano@gmail.com>
2026-01-19 14:53:46 +0300

    update spec to handle errors

--- src/specs/okhi-nitro.nitro.ts
+++ src/specs/okhi-nitro.nitro.ts
@@ -27,31 +27,53 @@ export interface OkhiNitro extends HybridObject<{
     ) => void
   ): void
 
-  isLocationServicesEnabled(callback: (result: boolean) => void): void
-
-  canOpenProtectedApps(callback: (result: boolean) => void): void
-
-  getLocationAccuracyLevel(callback: (result: string) => void): void
-
-  isBackgroundLocationPermissionGranted(
-    callback: (result: boolean) => void
+  isLocationServicesEnabled(
+    callback: (result?: boolean, error?: OkHiException) => void
   ): void
 
-  isCoarseLocationPermissionGranted(callback: (result: boolean) => void): void
+  canOpenProtectedApps(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
 
-  isFineLocationPermissionGranted(callback: (result: boolean) => void): void
+  getLocationAccuracyLevel(
+    callback: (result?: string, error?: OkHiException) => void
+  ): void
 
-  isPlayServicesAvailable(callback: (result: boolean) => void): void
+  isBackgroundLocationPermissionGranted(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
 
-  isPostNotificationPermissionGranted(callback: (result: boolean) => void): void
+  isCoarseLocationPermissionGranted(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
+
+  isFineLocationPermissionGranted(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
+
+  isPlayServicesAvailable(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
+
+  isPostNotificationPermissionGranted(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
 
   openProtectedApps(): void
 
-  requestBackgroundLocationPermission(callback: (result: boolean) => void): void
+  requestBackgroundLocationPermission(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
 
-  requestEnableLocationServices(callback: (result: boolean) => void): void
+  requestEnableLocationServices(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
 
-  requestLocationPermission(callback: (result: boolean) => void): void
+  requestLocationPermission(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
 
-  requestPostNotificationPermissions(callback: (result: boolean) => void): void
+  requestPostNotificationPermissions(
+    callback: (result?: boolean, error?: OkHiException) => void
+  ): void
 }
