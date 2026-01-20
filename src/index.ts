02bf9f767408b1b14f7fe08ba5b87f307e0a5ca3
jgkiano <jgkiano@gmail.com>
2026-01-20 06:04:15 +0300

    expose helper methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { OkHiNitro, startGenericAddressVerification } from './functions'
-import type { OkCollect, OkHiLogin } from './types'
+import type { OkCollect, OkHiException, OkHiLogin, OkHiSuccessResponse } from './types'
 
 export async function login(
   credentials: OkHiLogin
@@ -11,22 +11,184 @@ export async function login(
 
 export async function startAddressVerification(params?: {
   okcollect?: OkCollect
-}) {
+}): Promise<OkHiSuccessResponse> {
   return startGenericAddressVerification('DIGITAL', params)
 }
 
 export async function startPhysicalAddressVerification(params?: {
   okcollect?: OkCollect
-}) {
+}): Promise<OkHiSuccessResponse> {
   return startGenericAddressVerification('PHYSICAL', params)
 }
 
 export async function startDigitalAndPhysicalAddressVerification(params?: {
   okcollect?: OkCollect
-}) {
+}): Promise<OkHiSuccessResponse> {
   return startGenericAddressVerification('DIGITALANDPHYSICAL', params)
 }
 
-export async function createAddress(params?: { okcollect?: OkCollect }) {
+export async function createAddress(params?: { okcollect?: OkCollect }): Promise<OkHiSuccessResponse> {
   return startGenericAddressVerification('ADDRESSBOOK', params)
+}
+
+export async function isLocationServicesEnabled(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.isLocationServicesEnabled((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function canOpenProtectedApps(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.canOpenProtectedApps((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function getLocationAccuracyLevel(): Promise<"precise" | "no_permission" | "approximate"> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.getLocationAccuracyLevel((result, error) => {
+      if (typeof result === "string") {
+        resolve(result.toLowerCase() as any)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function isBackgroundLocationPermissionGranted(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.isBackgroundLocationPermissionGranted((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function isCoarseLocationPermissionGranted(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.isCoarseLocationPermissionGranted((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function isFineLocationPermissionGranted(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.isFineLocationPermissionGranted((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function isPlayServicesAvailable(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.isPlayServicesAvailable((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function isPostNotificationPermissionGranted(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.isPostNotificationPermissionGranted((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function openProtectedApps(): Promise<void> {
+  return new Promise(() => {
+    return OkHiNitro.openProtectedApps()
+  });
+}
+
+export async function requestBackgroundLocationPermission(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.requestBackgroundLocationPermission((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function requestEnableLocationServices(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.requestEnableLocationServices((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function requestLocationPermission(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.requestLocationPermission((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
+}
+
+export async function requestPostNotificationPermissions(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    OkHiNitro.requestPostNotificationPermissions((result, error) => {
+      if (typeof result === "boolean") {
+        resolve(result)
+      } else {
+        const err: OkHiException = error || {code: "unknown", message: "unable to retrive result"}
+        reject(err)
+      }
+    })
+  });
 }
