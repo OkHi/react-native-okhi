01673985478c8b12373e25bb1a60093b0dd65f2f
jgkiano <jgkiano@gmail.com>
2026-07-23 09:29:09 +0300

    added closeAddressCollection obj method

--- ios/Okhi.mm
+++ ios/Okhi.mm
@@ -49,6 +49,14 @@ RCT_EXPORT_MODULE(Okhi)
   }];
 }
 
+- (void)closeAddressCollection:(RCTResponseSenderBlock)callback {
+  [OkHiWrapper closeAddressCollectionWithCallback:^(NSDictionary *error) {
+    if (callback) {
+      callback(@[error ?: [NSNull null]]);
+    }
+  }];
+}
+
 // MARK: - Helper Methods
 
 - (void)isLocationServicesEnabled:(RCTResponseSenderBlock)callback {
